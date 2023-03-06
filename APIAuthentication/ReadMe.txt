2a53990dd5e3bfec7d8763ff59c3e730668879bb
itsamankrsingh <itsamankrsingh@gmail.com>
2023-03-07 00:06:38 +0530

    Added Common.cs class to generate hashed password

--- APIAuthentication/ReadMe.txt
+++ APIAuthentication/ReadMe.txt
@@ -2,4 +2,5 @@
 2.) Add a Register Controller Class
 3.) Next we are working with existing DB, so we need to Scaffold DB, install 'EntityFrameworkCore' and 'entityframeworkcore.tools' and 'Microsoft.EntityFrameworkCore.SqlServer' package
 4.) Tools -> Nuget Package Manager -> Package Manager Console
-5.) Scaffold-DbContext "Server=localhost;Database=NetCoreAuthentication;user=sa;password=sa@123" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models to add existing db to this project
+5.) Scaffold-DbContext "Server=localhost;Database=NetCoreAuthentication;user=sa;password=sa@123" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models ( run this command in package manager console to add existing db to this project)
+6.) Cretat Folder Utils and Class Common.cs in it.
---
+++ APIAuthentication/Utils/Common.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography;
+
+namespace APIAuthentication.Utils
+{
+    public class Common
+    {
+        /*
+         * Function to create random salt string
+         * 
+         * 
+         */
+        public static byte[] GetRandomSalt(int length)
+        {
+            var random = RandomNumberGenerator.Create();
+            byte[] salt = new byte[length];
+            random.GetNonZeroBytes(salt);
+            return salt;
+        }
+
+        /*
+         * Function to create password with salt
+         * 
+         * 
+         */
+        public static byte[] SaltHashPassword(byte[] password, byte[] salt)
+        {
+            SHA256 algorithm = SHA256.Create();
+            byte[] plainTextWithSaltBytes = new byte[password.Length + salt.Length];
+            for (int i = 0; i < password.Length; i++)
+            {
+                plainTextWithSaltBytes[i] = password[i];
+            }
+            for (int i = 0; i < salt.Length; i++)
+            {
+                plainTextWithSaltBytes[password.Length + i] = salt[i];
+            }
+            return algorithm.ComputeHash(plainTextWithSaltBytes);
+        }
+
+    }
+}
